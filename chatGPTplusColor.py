f58b415ce4f9304910ea9e76f7f3007e66bd5d19
ManseJang <110883547+ManseJang@users.noreply.github.com>
2023-05-25 22:15:27 +0900

    Update chatGPTplusColor.py

--- chatGPTplusColor.py
+++ chatGPTplusColor.py
@@ -13,16 +13,12 @@ if submit and user_input:
     gpt_prompt = [{
         "role": "system",
         "content": "The assistant's job is to recommend color codes that match what user's describing.",
-        "role": "user",
-        "content": "A sunset beach",
-        "role": "assistant",
-        "content": "#000000, #000000, #000000, #000000, #000000"
     }]
 
 
     gpt_prompt.append({
         "role": "user",
-        "content": user_input
+        "content": user_input + ' hex color'
     })
 
     with st.spinner("Waiting for chatGPT..."):
@@ -32,11 +28,13 @@ if submit and user_input:
         )
 
     prompt = gpt_response["choices"][0]["message"]["content"]
+    #st.write(prompt)
     for line in prompt.split(' '):
         if '#' in line:
             #st.write(line[:7])
             color = line[:7]
             st.markdown(f'<h1 style="color:{color};font-size:24px;">{color}</h1>', unsafe_allow_html=True)
         else :
-            st.write('We can\'t find color. please enter different prompt.')
-            break
+            continue
+    if '#' not in prompt:
+        st.write('GPT does not have a color recommendation, Please enter another prompt.')
